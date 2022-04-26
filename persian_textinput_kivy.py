a888b34363e0e02bd3f86fad7cb96068f5b00918
uak <411046+damascene@users.noreply.github.com>
2022-04-26 06:02:07 +0300

    Create ready to run example
    Hey, I've seen your code referenced in different questions on the internet. I've made a ready to run example of it.

--- persian_textinput_kivy.py
+++ persian_textinput_kivy.py
@@ -1,19 +1,27 @@
-# you must have tow library named python_bidi and arabic_reshper
+import arabic_reshaper
+from bidi.algorithm import get_display
 
+from kivy.uix.textinput import TextInput
 
-# in .kv file
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import (
+                            StringProperty,
+                            NumericProperty
+                            )
 
+KV = '''
 <Fa_text@TextInput>:
     text: "efevresi.com"
     multiline: 0
-    font_name: "data/BBadr.ttf" # the font you want to use
+    font_name: "/usr/share/fonts/opentype/fonts-hosny-amiri/Amiri-Regular.ttf" # the font you want to use
     font_size: 26
     padding_y: [6,0] # can be changed
     padding_x: [self.size[0]-self._get_text_width(max(self._lines, key=len), self.tab_width, self._label_cached)-10,8]
+'''
 
 
 
-# in .py class part:
 
 class Fa_text(TextInput):
     max_chars = NumericProperty(20) # maximum character allowed
@@ -30,4 +38,12 @@ class Fa_text(TextInput):
     def do_backspace(self, from_undo=False, mode='bkspc'):
         self.str = self.str[0:len(self.str)-1]
         self.text = get_display(arabic_reshaper.reshape(self.str))
-
+        
+class TestApp(App):
+    def build(self):
+        self.count = 0
+        Builder.load_string(KV)
+        return Fa_text()
+        
+if __name__ == '__main__':
+    TestApp().run()
